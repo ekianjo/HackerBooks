8a7e2d343ce81d67daf211eff4ecb25de674e6d6
ekianjo <ekianjo@gmail.com>
2013-05-13 17:41:24 +0900

    Create main.py
    outline

---
+++ main.py
@@ -0,0 +1,6 @@
+#overview
+#first: window with tree like list of all books and directory to download the books to.  
+#clicking on book name shows info about book + cover with 2 buttons: download / back to list
+#download triggers wget event and launches zenity script to show progress. 
+
+
